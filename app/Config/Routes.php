99748983f65a6d2400cfcfdf847dea60630ae018
Hafidz Alhakim <hafidz21si@mahasiswa.pcr.ac.id>
2025-07-07 21:25:40 +0700

    penambahan fitur yang belum selesai

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,7 +9,7 @@ use CodeIgniter\Router\RouteCollection;
 
 // $routes->get('/', 'Home::index');
 // halaman login
-$routes->get('/', 'LoginController::login');
+$routes->get('/login', 'LoginController::login');
 // log out
 $routes->get('logout', 'LoginController::logout');
 // validasi masuk
