805dd1508890818e768d705042474dcf4fdb74f4
RaihanJodya <raihan21si@mahasiswa.pcr.ac.id>
2025-07-20 07:11:26 +0700

    tambahan tampilan dashboard

--- app/Views/divisiict/index2.php
+++ app/Views/divisiict/index2.php
@@ -3,34 +3,21 @@
   <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
 
     <title>ICT Request-DivisiICT</title>
 
     <!-- Custom fonts for this template -->
-    <link
-      href=<?=base_url('assets/vendor/fontawesome-free/css/all.min.css')?>
-      rel="stylesheet"
-      type="text/css"
-    />
-    <link
-      href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-      rel="stylesheet"
-    />
+    <link href=<?=base_url('assets/vendor/fontawesome-free/css/all.min.css')?> rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
 
     <!-- Custom styles for this template -->
     <link href=<?=base_url('assets/css/sb-admin-2.min.css')?> rel="stylesheet" />
 
     <!-- Custom styles for this page -->
-    <link
-      href=<?=base_url('assets/vendor/datatables/dataTables.bootstrap4.min.css')?>
-      rel="stylesheet"
-    />
+    <link href=<?=base_url('assets/vendor/datatables/dataTables.bootstrap4.min.css')?> rel="stylesheet" />
     <!-- Custom Warna Table -->
     <link rel="stylesheet" href=<?=base_url('assets/css/table.css')?>>
   </head>
@@ -42,11 +29,11 @@
        <ul class="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
 
         <!-- Sidebar - Brand -->
-        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= site_url('divisi-ict') ?>">
             <div class="sidebar-brand-icon">
-            <i class="fas fa-list-ul"></i>
+            <i class="fas fa-list-ul text-dark"></i>
             </div>
-            <div class="sidebar-brand-text mx-3">Divisi ICT Request</sup></div>
+            <div class="sidebar-brand-text mx-3 text-white">Divisi ICT Request</div>
         </a>
 
         <!-- Divider -->
@@ -100,56 +87,22 @@
         <!-- Main Content -->
         <div id="content">
           <!-- Topbar -->
-          <nav
-            class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow"
-          >
-            <!-- Sidebar Toggle (Topbar) -->
+          <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
             <form class="form-inline">
-              <button
-                id="sidebarToggleTop"
-                class="btn btn-link d-md-none rounded-circle mr-3"
-              >
+              <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                 <i class="fa fa-bars"></i>
               </button>
             </form>
-
-            <!-- Topbar Navbar -->
             <ul class="navbar-nav ml-auto">
               <div class="topbar-divider d-none d-sm-block"></div>
-
-              <!-- Nav Item - User Information -->
               <li class="nav-item dropdown no-arrow">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="userDropdown"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <span class="mr-2 d-none d-lg-inline text-gray-600 small"
-                    ><?= session()->get('username')  ?></span
-                  >
-                  <img
-                    class="img-profile rounded-circle"
-                    src=<?=base_url('assets/img/undraw_profile.svg')?>
-                  />
+                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= session()->get('username') ?></span>
+                  <img class="img-profile rounded-circle" src=<?=base_url('assets/img/undraw_profile.svg')?> />
                 </a>
-                <!-- Dropdown - User Information -->
-                <div
-                  class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                  aria-labelledby="userDropdown"
-                >
-                  <a
-                    class="dropdown-item"
-                    href=<?=base_url('/logout')?>
-                    data-toggle="modal"
-                    data-target="#logoutModal"
-                  >
-                    <i
-                      class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"
-                    ></i>
+                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                  <a class="dropdown-item" href=<?=base_url('/logout')?> data-toggle="modal" data-target="#logoutModal">
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                     Logout
                   </a>
                 </div>
@@ -160,19 +113,60 @@
 
           <!-- Begin Page Content -->
           <div class="container-fluid">
-            <!-- Page Heading -->
             <h1 class="h3 mb-2 text-gray-800">Inbox Request</h1>
 
-            <!-- DataTales Example -->
+            <div class="row mb-4">
+              <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                  <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                      <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Request</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">23</div>
+                      </div>
+                      <div class="col-auto">
+                        <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-success shadow h-100 py-2">
+                  <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                      <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Request Selesai</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">15</div>
+                      </div>
+                      <div class="col-auto">
+                        <i class="fas fa-check-circle fa-2x text-gray-300"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-warning shadow h-100 py-2">
+                  <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                      <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Total Barang</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">42</div>
+                      </div>
+                      <div class="col-auto">
+                        <i class="fas fa-boxes fa-2x text-gray-300"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
             <div class="card shadow mb-4">
               <div class="card-body">
                 <div class="table-responsive">
-                  <table
-                    class="table table-bordered"
-                    id="dataTable"
-                    width="100%"
-                    cellspacing="0"
-                  >
+                  <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                       <tr>
                         <th>Name</th>
@@ -192,11 +186,10 @@
                         <td><?= $reques->jpermintaan?></td>
                         <td><?= $reques->description?></td>
                         <td><?= $reques->date_request?></td>
-                        <td class=" d-flex align-items-center justify-content-center"><a href=<?php echo site_url('rincian-request-ict').'/'.$reques->id_request?>><button class="btn btn-link" style="background-color: black; color: white;">Assign</button></a></td>
-                        <td class="  align-items-center justify-content-center"><a href=<?php echo site_url('data-print-rincian').'/'.$reques->id_request?>><button class="btn btn-link" style="background-color: black; color: white;">Print</button></a></td>
+                        <td class="d-flex align-items-center justify-content-center"><a href=<?= site_url('rincian-request-ict').'/'.$reques->id_request ?>><button class="btn btn-link" style="background-color: black; color: white;">Assign</button></a></td>
+                        <td class="align-items-center justify-content-center"><a href=<?= site_url('data-print-rincian').'/'.$reques->id_request ?>><button class="btn btn-link" style="background-color: black; color: white;">Print</button></a></td>
                       </tr>
-                      <?php endforeach?>
-                      
+                    <?php endforeach?>
                     </tbody>
                   </table>
                 </div>
@@ -204,6 +197,7 @@
             </div>
           </div>
           <!-- /.container-fluid -->
+
         </div>
         <!-- End of Main Content -->
 
@@ -211,7 +205,7 @@
         <footer class="sticky-footer bg-white">
           <div class="container my-auto">
             <div class="copyright text-center my-auto">
-            <span>Copyright &copy; ICT Request <?php echo date("Y")?></span>
+              <span>Copyright &copy; ICT Request <?= date("Y") ?></span>
             </div>
           </div>
         </footer>
@@ -221,45 +215,23 @@
     </div>
     <!-- End of Page Wrapper -->
 
-    <!-- Scroll to Top Button-->
     <a class="scroll-to-top rounded" href="#page-top">
       <i class="fas fa-angle-up"></i>
     </a>
 
-    <!-- Logout Modal-->
-    <div
-      class="modal fade"
-      id="logoutModal"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">Apakah Anda Yakin Ingin Keluar?</h5>
-            <button
-              class="close"
-              type="button"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">×</span>
             </button>
           </div>
-          <div class="modal-body">
-          Jika yakin, silakan tekan tombol "Logout" yang ada dibawah!
-          </div>
+          <div class="modal-body">Jika yakin, silakan tekan tombol "Logout" yang ada dibawah!</div>
           <div class="modal-footer">
-            <button
-              class="btn btn-secondary"
-              type="button"
-              data-dismiss="modal"
-            >
-              Cancel
-            </button>
-            <a href=<?=base_url('/logout')?> class="btn btn-primary" >Logout</a>
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+            <a href=<?=base_url('/logout')?> class="btn btn-primary">Logout</a>
           </div>
         </div>
       </div>
@@ -279,10 +251,7 @@
     <script src=<?=base_url('assets/vendor/datatables/jquery.dataTables.min.js')?>></script>
     <script src=<?=base_url('assets/vendor/datatables/dataTables.bootstrap4.min.js')?>></script>
 
-    <!-- Page level custom scripts -->
-    <script src=<?=base_url('assets/js/demo/datatables-demo.js')?>></script>
-
-    <!-- script warna table -->
+    <!-- Page level custom scripts -->    <script src=<?=base_url('assets/js/demo/datatables-demo.js')?>></script>
     <script src=<?=base_url('assets/js/table.js')?>></script>
   </body>
 </html>
